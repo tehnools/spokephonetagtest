3a6ca813f54a587c892ee24b14bd1f99b984b767
noel <noelsoong@live.com>
2020-10-07 11:44:21 +1300

    wip: settings babelrc for jest

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
 const tagRegex = new RegExp('<\/*[A-Z]>', 'g')
+const tagCloseRegex = new RegExp('</[A-Z]>', 'g')
+const tagOpenRegex = new RegExp('<[A-Z]>', 'g')
 
 const SuccessMessage = 'Correctly tagged paragraph'
 
-export function generateClosingTag(tag) {
-    if (tag === '#') return tag
+export function openToCloseTag(tag) {
     return `</${tag.substr(1)}`
 }
 
-// export function generateStartTag(tag) {
-//     if (tag === '#') return tag
-//     return `<${tag.substr(2)}`
-// }
+export function closeToOpenTag(tag) {
+    if (tag === '#') return tag
+    return `<${tag.substr(2)}`
+}
 
-export function findTags(input) { 
+export function findTags(input) {
     return input.match(tagRegex)
 }
 
 // pivot
-function tagChecker(input){
-    let pointer
+function tagChecker(input) {
     // let open
-    // let expected
-    // let unexpected
+
     // let openTags = input.match(openRegex)
     // let closeTags = input.match(closeRegex).reverse()
     let tags = input.match(tagRegex)
     console.log('Tags', tags)
     for (let i; i < tags.length; i++) {
-        
+
     }
-    // while(openTags.length > 0 || closeTags.length > 0){
-    //     open = openTags.pop() || '#'
-    //     expected = generateClosingTag(open) || '#'
-    //     unexpected = closeTags.pop() || '#'
-    //     if(expected !== unexpected){
-    //        return `Expected ${expected} found ${unexpected}`
-    //     }
-    // }    
-    // return SuccessMessage    
+    while (tags.length > 0) {
+        let pointer = 0;
+        let expected = '#';
+        let found = '#';
+        const tag = tags.pop();
+        if (tag.match(tagOpenRegex)) {
+            expected = openToCloseTag(tag)
+        } else if (tag.match(tagCloseRegex)) {
+            found = tag
+        }
+        // open = openTags.pop() || '#'
+        // expected = generateClosingTag(open) || '#'
+        // found = closeTags.pop() || '#'
+        if (expected !== found) {
+            return `Expected ${expected} found ${found}`
+        }
+    }
+    return SuccessMessage
 }
 
 
 
-function main(){
+function main() {
     const test1 = 'The following text<C><B>is centred and in boldface</B></C>'
-    const test2 = '<B>This <\g>is <B>boldface</B> in <<*> a</B> <\6> <<d>sentence'
+    const test2 = '<B>This <\g>is <B>boldface</B> in <<*> a</B> <\\6> <<d>sentence'
     const test3 = '<B><C> This should be centred and in boldface, but thetagsare wrongly nested </B></C>'
     const test4 = '<B>This should be in boldface, but there is an extra closingtag</B></C>'
     const test5 = '<B><C>This should be centred and in boldface, but there isa missing closing tag</C>'
--- index.test.js
+++ index.test.js
@@ -1,50 +1,51 @@
-import { tagChecker, findTags} from './index'
+import { xtest } from '@jest/globals';
+import { tagChecker, findTags } from './index'
 
 describe('tagChecker', () => {
 
-    it('findTags function should be able to match tags', () => {
+    test('findTags function should be able to match tags', () => {
         const input = '<A></A><B></B>'
         const expected = ['<A>', '</A>', '<B>', '</B>']
         const result = findTags(input)
         expect(result).toBe(expected)
-    });  
+    });
 
-    it.skip('should ', () => {
+    test('should ', () => {
         const test = '<A><B></A></B>'
         const expected = 'Expected </A> found </B>'
         const result = tagChecker(test)
         expect(expected).toBe(result)
     });
 
-    it.skip('should ', () => {
+    xtest('should ', () => {
         const test = '<A><B></A></B>'
         const expected = 'Expected </A> found </B>'
         const result = tagChecker(test)
         expect(expected).toBe(result)
     });
-    
-    it.skip('should ', () => {
-        const test = '<B>This <\g>is <B>boldface</B> in <<*> a</B> <\6> <<d>sentence'
+
+    xtest('should ', () => {
+        const test = '<B>This <\g>is <B>boldface</B> in <<*> a</B> <\\6> <<d>sentence'
         const result = tagChecker(test)
         const expected = SuccessMessage
         expect(expected).toBe(result)
     });
-    
-    it.skip('should ', () => {
+
+    xtest('should ', () => {
         const test = '<B><C> This should be centred and in boldface, but thetagsare wrongly nested </B></C>'
         const result = tagChecker(test)
         const expected = 'Expected </C> found </B>'
         expect(result).toBe(expected)
     });
-    
-    it.skip('should ', () => {
+
+    xtest('should ', () => {
         const test = '<B>This should be in boldface, but there is an extra closingtag</B></C>'
         const result = tagChecker(test)
         const expected = 'Expected # found </C>'
         expect(result).toBe(expected)
     });
-    
-    it.skip('should ', () => {
+
+    xtest('should ', () => {
         const test = '<B><C>This should be centred and in boldface, but there isa missing closing tag</C>'
         const result = tagChecker(test)
         const expected = 'Expected </B> found #'
